577786b6ec9e282e73d7b903ece3a00dfd44bedd
Yannick <yannickzs@hotmail.com>
2016-10-29 00:42:27 +0200

    Player can shoot bullets

--- 2DShooter/Player.cs
+++ 2DShooter/Player.cs
@@ -13,18 +13,20 @@ namespace _2DShooter
 {
    public class Player
     {
-        public Texture2D texture;
+        public Texture2D texture, bulletTexture;
         public Vector2 position;
         public int speed;
-
-        // Variables for collisions
+        public float bulletDelay;
         public Rectangle boundingBox;
         public bool isColliding;
+        public List<Bullet> bulletList;
 
         public Player()
         {
+            bulletList = new List<Bullet>();
             texture = null;
             position = new Vector2(300, 300);
+            bulletDelay = 5;
             speed = 10;
             isColliding = false;
         }
@@ -32,11 +34,16 @@ namespace _2DShooter
         public void LoadContent(ContentManager Content)
         {
             texture = Content.Load<Texture2D>("ship");
+            bulletTexture = Content.Load<Texture2D>("playerbullet");
+
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(texture, position, Color.White);
+
+            foreach (Bullet b in bulletList)
+                b.Draw(spriteBatch);
         }
 
         public void Update(GameTime gameTime)
@@ -44,17 +51,24 @@ namespace _2DShooter
             KeyboardState keyState = Keyboard.GetState();
 
             //Controls
-            if (keyState.IsKeyDown(Keys.W))
+            if (keyState.IsKeyDown(Keys.Up) || keyState.IsKeyDown(Keys.W))
                 position.Y = position.Y - speed;
 
-            if (keyState.IsKeyDown(Keys.A))
+            if (keyState.IsKeyDown(Keys.Left) || keyState.IsKeyDown(Keys.A))
                 position.X = position.X - speed;
 
-            if (keyState.IsKeyDown(Keys.S))
+            if (keyState.IsKeyDown(Keys.Down) || keyState.IsKeyDown(Keys.S))
                 position.Y = position.Y + speed;
 
-            if (keyState.IsKeyDown(Keys.D))
+            if (keyState.IsKeyDown(Keys.Right) || keyState.IsKeyDown(Keys.D))
                 position.X = position.X + speed;
+
+            //Fire controls
+            if (keyState.IsKeyDown(Keys.Space))
+            {
+                Shoot();
+            }
+            UpdateBullets();
 
             // Make sure ship stays inbounds
 
@@ -69,6 +83,46 @@ namespace _2DShooter
 
             if (position.Y >= 950 - texture.Height)
                 position.Y = 950 - texture.Height;
+        }
+
+        public void Shoot()
+        {
+            if (bulletDelay >= 0)
+                bulletDelay--;
+                
+            if (bulletDelay <= 0)
+            {
+                Bullet newBullet = new Bullet(bulletTexture);
+                newBullet.position = new Vector2(position.X + 32 - newBullet.texture.Width / 2, position.Y + 30);
+
+                newBullet.isVisible = true;
+
+                if (bulletList.Count() < 20)
+                    bulletList.Add(newBullet);
+            }
+
+            if (bulletDelay == 0)
+                bulletDelay = 5;    
+        }
+
+        public void UpdateBullets()
+        {
+            foreach (Bullet b in bulletList)
+            {
+                b.position.Y = b.position.Y - b.speed;
+
+                if (b.position.Y <= 0)
+                    b.isVisible = false;
+            }
+
+            for (int i = 0; i < bulletList.Count; i++)
+            {
+                if (!bulletList[i].isVisible)
+                {
+                    bulletList.RemoveAt(i);
+                    i--;
+                }
+            }
         }
     }
 }
